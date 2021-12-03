34756035b6c789aee7db668fe5ae9422da3b1518
SkylineDrop <shas.o.r.myr@gmail.com>
2021-12-03 22:22:58 +0300

    add static

--- simpleapp/views.py
+++ simpleapp/views.py
@@ -1,9 +1,13 @@
-from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from .models import Product
 
 class ProductsList(ListView):
+    model = Product
+    template_name = 'products.html' 
+    context_object_name = 'products'
+
+class ProductDetail(DetailView):
     model = Product
     template_name = 'products.html' 
     context_object_name = 'products'
